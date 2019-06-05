397a446b7554d5ed8dae05f2546a0bbff44e4e14
Daniel Belarmino <belarba@gmail.com>
2019-06-05 15:33:49 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_06_05_171806) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,7 +79,6 @@ ActiveRecord::Schema.define(version: 2019_06_05_171806) do
     t.string "photo"
     t.float "latitude"
     t.float "longitude"
-    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
